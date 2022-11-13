f2db98f2d0dd5e4d0027c0cd81e3a6fe745f0733
adrielle <93674291+adrielle19@users.noreply.github.com>
2022-11-13 11:57:01 -0300

    mais cometarios

--- Pages.py
+++ Pages.py
@@ -1,8 +1,8 @@
 class Page:
     def __init__(self, Process, RecentlyUsed = 50):
-        self.Process = Process
-        self.RecentlyUsed = RecentlyUsed
-        self.VirtualMemoryAddress = -1
+        self.Process = Process # processo armazenado na pagina
+        self.RecentlyUsed = RecentlyUsed # contador para ultima vez que foi utilizado
+        self.VirtualMemoryAddress = -1 # referencia a memoria virtual
 
 class VirtualPage:
     def __init__(self, Process):
