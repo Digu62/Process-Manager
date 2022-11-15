8167c3781edd76d4c194d0f071fe28a7b787f913
Digu62 <rodrigomatospeixoto@gmail.com>
2022-11-15 12:45:39 -0300

    .

--- func_main.py
+++ func_main.py
@@ -1,5 +1,6 @@
-from tkinter import *
 import math
+from tkinter import *
+from tkinter import ttk
 
 def open_win(window,p,q,o):
 
@@ -10,15 +11,16 @@ def open_win(window,p,q,o):
     # frame
     main_frame = Frame(root)
     main_frame.pack(fill=BOTH, expand=1)
+    main_frame.configure(bg='#569BAA')
 
     #canvas 
     canvas = Canvas(main_frame)
     canvas.pack(side=LEFT, fill=BOTH, expand=1)
+    canvas.configure(bg='#569BAA')
 
     #scroll
     scroll = Scrollbar(main_frame, orient=VERTICAL, command=canvas.yview)
     scroll.pack(side=RIGHT, fill=Y)
-
 
     #canvas config
     canvas.configure(yscrollcommand=scroll.set)
@@ -26,6 +28,7 @@ def open_win(window,p,q,o):
 
     #segundo frame
     frame2 = Frame(canvas)
+    frame2.configure(bg='#569BAA')
     #new frame -> canvas
     canvas.create_window((0,0), window=frame2, anchor='n')
 
@@ -33,7 +36,7 @@ def open_win(window,p,q,o):
     j=0
     for i in range(0,p):
 
-        Label(frame2, text=f'Processo(Id): {i}').grid(row=j, column=0, sticky= N, pady=(0,7), padx=(0,20))    
+        lb1 = Label(frame2, text=f'Processo(Id): {i}').grid(row=j, column=0, sticky= N, pady=(0,7), padx=(0,20))
         Entry(frame2, text=f'Processo(Id): {i}').grid(row=j, column=1, sticky= N, pady=(0,7), padx=(0,20))
 
         Label(frame2, text=f'Inicio do processo:').grid(row=j+1, column=0, sticky= N, pady=(0,7), padx=(0,20))  
@@ -86,7 +89,7 @@ def memoryWindow():
         for j in range(n_columns):
             table = Entry(memory_window, width=3, fg='black',
                             font=('Arial',16,'bold'))
-            table.grid(row=i, column=j,padx=-1, pady=0) 
+            table.grid(row=i, column=j) 
             table.insert(END,lst[i][j])
 
     for i in range(SecondHalf,n_rows):
@@ -122,6 +125,64 @@ def memoryWindow():
     
         
     memory_window.mainloop()
+
+def processWindow():
+    process_window= Tk()
+    process_window.title('Escalonador de Processos')
+    process_window.geometry("800x800+300+0")
+    process_window.configure(bg='#569BAA')
+
+# Creating table
+    n_rows = 10 #Will receive number of process
+    n_columns = 50 #Will receive time spend to compute all process
+    counter = 0
+
+# Table o progress
+    for i in range(n_rows):
+        for j in range(n_columns):
+            table = Entry(process_window, width=3, fg='black',
+                            font=('Arial',16,'bold'))
+            table.grid(row=i, column=j+10) 
+            table.configure({"background":'Green'})
+
+#Table with process informations
+    #------
+
+# Creating ruller
+    #------
+
+# Creating progress bar
+    def val_bar(max):
+        count = 0
+        steps = max/100
+        print(steps)
+        while count < steps:
+            count+=1
+            i=0
+            while i < 1000000: #Velocity from progress update
+                i+=1
+            
+            if count == 50:
+                progress_bar.config(style='red.Horizontal.TProgressbar')
+            elif count == 80:
+                progress_bar.config(style='green.Horizontal.TProgressbar')
+            var_progress.set(count)
+            process_window.update()
+
+
+    var_progress = DoubleVar()
+    st = ttk.Style()
+    st.theme_use('alt')
+    st.configure('red.Horizontal.TProgressbar', background='red')
+    st.configure('green.Horizontal.TProgressbar', background='green')
+    progress_bar = ttk.Progressbar(process_window, variable=var_progress, maximum=100, style='green.Horizontal.TProgressbar')
+    progress_bar.place(x=0,y=29,width=620,height=27) #grid(1) == width(40)
+    val_bar(100000)
+
+#Creating labels information
+    #------
+
+    process_window.mainloop()
 
 
 #def process_run(window, )
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from tkinter import *
 from func_main import *
 
-memoryWindow()
-exit()
+# processWindow()
+# # memoryWindow()
+# exit()
 
 window = Tk()
 window.title('Escalonador de Processos e Memória')
@@ -14,25 +15,31 @@ lbl1 = Label(window, text='Número de processos', anchor='center')
 lbl1.place(x=70, y=120)
 ent1 = Entry(justify='center')
 ent1.place(x=200, y=120)
+lbl1.configure(bg='#569BAA')
         
 lbl2 = Label(window, text='Quantum do sistema', anchor='center')
 lbl2.place(x=70, y=150)
 ent2 = Entry(justify='center')
 ent2.place(x=200, y=150)
+lbl2.configure(bg='#569BAA')
 
 lbl3 = Label(window, text='Sobrecarga do sistema', anchor='center')
 lbl3.place(x=70, y=180)
 ent3 = Entry(justify='center')
 ent3.place(x=200, y=180)
+lbl3.configure(bg='#569BAA')
 
 teachlbl = Label(window, text='Professor: Maycon')
 teachlbl.place(x=0, y=312)
+teachlbl.configure(bg='#569BAA')
 
 discplbl = Label(window, text='Disciplina: Sistemas Operacionais')
 discplbl.place(x=0, y=330)
+discplbl.configure(bg='#569BAA')
 
 teamlbl = Label(window, text='Equipe: Adrielle, Fernando, Igor, Rodrigo')
 teamlbl.place(x=0, y=348)
+teamlbl.configure(bg='#569BAA')
 
 def call_open():
     p = int(ent1.get())
